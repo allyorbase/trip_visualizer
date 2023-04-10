ad4c2a858d5212d98f330480e25e8f586a736b71
Ethan <git@nyant.us>
2023-04-09 19:38:55 -0700

    Info window adjustments and some statistics

--- frontend/main.js
+++ frontend/main.js
@@ -18,6 +18,8 @@ let openRouteServiceTokens = rateLimitMaxPerApi;
 let nominatimTokens = rateLimitMaxPerApi;
 let lastOpenRouteServiceTokenRefill = Date.now();
 let lastNominatimTokenRefill = Date.now();
+var tripInfo;
+
 
 async function loadTokens() {
   const response = await fetch('creds.json');
@@ -69,7 +71,7 @@ const markerSource = new VectorSource();
 async function init() {
   const response = await fetch('trips.json');
   const data = await response.json();
-  const tripInfo = data.payload.tripInfo;
+  tripInfo = data.payload.tripInfo;
 
   // Fetch addresses for start and end locations
   for (const trip of tripInfo) {
@@ -78,6 +80,17 @@ async function init() {
     trip.startLocation.address = await reverseGeocode(startCoord);
     trip.endLocation.address = await reverseGeocode(endCoord);
   }
+
+  updateStats();
+
+  document.getElementById("stats-dropdown-button").addEventListener("click", () => {
+    const content = document.getElementById("stats-dropdown-content");
+    content.classList.toggle("show");
+  });
+
+  document.getElementById('close-btn').addEventListener('click', hideOverlay);
+  document.getElementById('overlay-container').addEventListener('click', hideOverlay);
+
 
   // Add markers and routes
   const markerFeatures = [];
@@ -91,6 +104,10 @@ async function init() {
       geometry: new Point(fromLonLat(startCoord)),
       name: 'Start',
       tripData: trip,
+    });
+    startMarker.on("click", () => {
+      showOverlay(generateTripInfoHTML(trip));
+      dimOtherRoutes(i);
     });
     const startMarkerStyle = new Style({
       image: new Circle({
@@ -113,6 +130,10 @@ async function init() {
       name: 'End',
       tripData: trip,
     });
+    endMarker.on("click", () => {
+      showOverlay(generateTripInfoHTML(trip));
+      dimOtherRoutes(i);
+    });
     const endMarkerStyle = new Style({
       image: new Circle({
         radius: 8,
@@ -131,7 +152,7 @@ async function init() {
     // Draw route and push the resulting Feature to routeFeatures
     const routeFeature = await drawRoute(startCoord, endCoord, i);
     routeFeature.on('click', () => {
-      showOverlay(generateTripInfoHTML(i));
+      showOverlay(generateTripInfoHTML(tripInfo[i]));
       dimOtherRoutes(i);
     });
     routeFeatures.push(routeFeature);
@@ -160,9 +181,46 @@ async function init() {
       return orangeLineStyle;
     },
   });
+  map.on("click", (event) => {
+    const features = map.getFeaturesAtPixel(event.pixel);
+    if (features && features.length > 0) {
+      const routeFeature = features.find(
+        (feature) => feature.get("index") !== undefined
+      );
+      if (routeFeature) {
+        const index = routeFeature.get("index");
+        const routeCoord = routeFeature.getGeometry().getLastCoordinate();
+        showOverlay(generateTripInfoHTML(index), map.getPixelFromCoordinate(routeCoord));
+        dimOtherRoutes(index);
+      }
+    } else {
+      resetDimmedRoutes();
+      hideOverlay();
+    }
+  });
+  const closeButton = document.querySelector('#close-btn');
+  closeButton.addEventListener('click', () => {
+    // Hide the address overlay
+    addressOverlay.setPosition(undefined);
+    closeButton.blur();
+    return false;
+  });
 
   map.addLayer(routeLayer);
   map.addLayer(markerLayer);
+}
+
+function updateStats() {
+  const totalTrips = tripInfo.length;
+  const totalMiles = tripInfo.reduce((sum, trip) => sum + trip.milesDriven, 0);
+  const avgTripDistance = totalMiles / totalTrips;
+  // Calculate other statistics similarly and update the innerHTML of the stats-dropdown-content
+  document.getElementById("stats-dropdown-content").innerHTML = `
+    <p>Total Trips: ${totalTrips}</p>
+    <p>Total Miles: ${totalMiles.toFixed(2)}</p>
+    <p>Average Trip Distance: ${avgTripDistance.toFixed(2)} miles</p>
+    <!-- Add other statistics here -->
+  `;
 }
 
 function refillTokens() {
@@ -206,6 +264,51 @@ function showTripInfo(tripData) {
   const addressOverlay = document.getElementById('address-overlay');
   addressOverlay.innerHTML = `<h3>${tripData.startTime}</h3> <p>${tripData.startLocation.address}</p> <h3>${tripData.endTime}</h3> <p>${tripData.endLocation.address}</p>`;
   addressOverlay.style.display = 'block';
+}
+
+function generateTripInfoHTML(index) {
+  const trip = tripInfo[index];
+  const startAddress = trip.startLocation.address;
+  const endAddress = trip.endLocation.address;
+  const startTime = trip.startDtTime;
+  const endTime = trip.endDtTime;
+  const milesDriven = trip.milesDriven.toFixed(2);
+  const duration = `${trip.hoursDriven}h ${trip.minutesDriven}m ${trip.secondsDriven}s`;
+  const avgSpeed = trip.aveSpeed.toFixed(2);
+
+  return `
+    <h3>Start: ${startTime}</h3>
+    <p>${startAddress}</p>
+    <h3>End: ${endTime}</h3>
+    <p>${endAddress}</p>
+    <p><strong>Miles Driven:</strong> ${milesDriven}</p>
+    <p><strong>Duration:</strong> ${duration}</p>
+    <p><strong>Average Speed:</strong> ${avgSpeed} mph</p>
+  `;
+}
+
+function dimOtherRoutes(selectedIndex) {
+  routeSource.getFeatures().forEach((feature) => {
+    const index = feature.get("index");
+    const style = feature.getStyle();
+    if (index !== selectedIndex) {
+      style.getStroke().setColor("rgba(255, 255, 255, 0.2)");
+      feature.getElement().classList.add("dimmed"); /* Added to add dimmed class to element */
+    } else {
+      style.getStroke().setColor(getOrangeHue(index));
+      feature.getElement().classList.remove("dimmed"); /* Added to remove dimmed class from element */
+    }
+    feature.setStyle(style);
+  });
+}
+
+function resetDimmedRoutes() {
+  routeSource.getFeatures().forEach((feature) => {
+    const index = feature.get("index");
+    const style = feature.getStyle();
+    style.getStroke().setColor(getOrangeHue(index));
+    feature.setStyle(style);
+  });
 }
 
 async function drawRoute(coord1, coord2, index) {
@@ -283,16 +386,21 @@ async function reverseGeocode(coord) {
   return addressText;
 }
 
-function showOverlay(tripInfo) {
-  document.getElementById('overlay-container').style.display = 'flex';
-  document.getElementById('overlay-content').innerHTML = tripInfo;
+function showOverlay(tripInfo, position) {
+  const overlayContainer = document.getElementById("overlay-container");
+  overlayContainer.style.display = "flex";
+  const overlayContent = document.getElementById("overlay-content");
+  const overlay = document.getElementById("overlay");
+  overlay.style.top =`${position[1]}px`;
+  overlay.style.left =`${position[0]}px`; 
+  overlayContent.innerHTML = tripInfo;
 }
 
 function hideOverlay() {
   document.getElementById('overlay-container').style.display = 'none';
+  resetDimmedRoutes();
 }
 
-document.getElementById('overlay-close').addEventListener('click', hideOverlay);
 
 map.on('singleclick', async function (evt) {
   const clickedFeature = map.forEachFeatureAtPixel(evt.pixel, function (feature) {
